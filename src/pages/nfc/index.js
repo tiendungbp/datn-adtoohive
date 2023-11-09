328734ae8a951f186c80fbd2004e316eee004690
dungnguyendev <dungnguyentien.dev@gmail.com>
2023-11-09 23:31:05 +0700

    nfc ok chuaw

--- src/pages/nfc/index.js
+++ src/pages/nfc/index.js
@@ -7,24 +7,24 @@ export default function NFC() {
 //   const [id,setid] = useState(0)
   const handleWriteNFC = async () => {
 
-    // alert("User clicked write button");
-    // // I want to write a URL to the NFC
-    // try {
-    //   const ndef = new NDEFReader();
+    alert("User clicked write button");
+    // I want to write a URL to the NFC
+    try {
+      const ndef = new NDEFReader();
 
-    //   await ndef.write({
-    //     records: [
-    //       {
-    //         recordType: "url",
-    //         data: "https://www.youtube.com/shorts/YTvUC3bIRpY",
-    //       },
-    //     ],
-    //   });
+      await ndef.write({
+        records: [
+          {
+            recordType: "url",
+            data: "https://www.youtube.com/shorts/YTvUC3bIRpY",
+          },
+        ],
+      });
 
-    //   alert("> Message written");
-    // } catch (error) {
-    //   alert("Argh! Cannot read data from the NFC tag. Try another one?");
-    // }
+      alert("> Message written");
+    } catch (error) {
+      alert("Argh! Cannot read data from the NFC tag. Try another one?");
+    }
   };
 
   return (
