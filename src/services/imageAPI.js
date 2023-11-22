df027a72f656b468258aee08f113a42dcf82ea7d
Nguyen Duy Hai <nguyenduyhai308@gmail.com>
2023-11-22 13:10:49 +0700

    fix

--- src/services/imageAPI.js
+++ src/services/imageAPI.js
@@ -1,9 +1,12 @@
-import axios from "axios";
+import axios from 'axios';
 
 const imageAPI = {
-    uploadImageToCloud: (formData) => {
-        return axios.post("https://api.cloudinary.com/v1_1/dcteqnz2i/image/upload", formData);
-    },
+	uploadImageToCloud: (formData) => {
+		return axios.post(
+			'https://api.cloudinary.com/v1_1/deldfzq9e/image/upload',
+			formData,
+		);
+	},
 };
 
 export default imageAPI;
